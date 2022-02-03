fd3299117bd37d145b57644c58c2e20432f2a7fa
Michael Hodgson <michael.hodgson@springboardit.co.uk>
2022-02-03 15:07:20 +0000

    Add bootswatch theme

---
+++ Jungle Books/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using Jungle_Books.Data;
+using Jungle_Books.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jungle_Books.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;   
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+    }
+}
