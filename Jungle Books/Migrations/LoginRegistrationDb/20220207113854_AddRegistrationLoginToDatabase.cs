ac6f0e41afb963f844802e0ab597d9e475707183
Michael Hodgson <michael.hodgson@springboardit.co.uk>
2022-02-07 11:40:11 +0000

    Migrate loginRegistration table to local sql server

---
+++ Jungle Books/Migrations/LoginRegistrationDb/20220207113854_AddRegistrationLoginToDatabase.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Jungle_Books.Migrations.LoginRegistrationDb
+{
+    public partial class AddRegistrationLoginToDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "loginRegistrations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_loginRegistrations", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "loginRegistrations");
+        }
+    }
+}
